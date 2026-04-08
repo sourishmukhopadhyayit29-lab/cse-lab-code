254218fe63a7ee4d914b651ebcd0bd96e1e57d18
sourishmukhopadhyayit29-lab <sourish.mukhopadhyay.it29@heritageit.edu.in>
2026-04-08 13:51:19 +0530

    Create Array4.c

---
+++ Array4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], b[10][10], sum[10][10];
+    int r1,c1,r2,c2,i,j;
+
+    scanf("%d%d",&r1,&c1);
+    scanf("%d%d",&r2,&c2);
+
+    if(r1!=r2 || c1!=c2){
+        printf("Addition not possible");
+        return 0;
+    }
+
+    for(i=0;i<r1;i++)
+        for(j=0;j<c1;j++)
+            scanf("%d",&a[i][j]);
+
+    for(i=0;i<r2;i++)
+        for(j=0;j<c2;j++)
+            scanf("%d",&b[i][j]);
+
+    for(i=0;i<r1;i++){
+        for(j=0;j<c1;j++){
+            sum[i][j]=a[i][j]+b[i][j];
+            printf("%d ",sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
